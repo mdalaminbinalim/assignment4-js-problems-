69843b07f0a9867ed28579f61c9ba154a62b1add
mdalaminbinalim <alamin29107@gmail.com>
2023-02-07 07:11:35 +0600

    Problem 5 solved

--- assignment-problems.js
+++ assignment-problems.js
@@ -4,10 +4,13 @@
 // /* Problem 1 */ //
 
 function mindGame (numberTaken){
+
     // This Function will return a number after some mathematical operations. //
+
     if (typeof numberTaken !== 'number'){
         return "Provide Valid Input. Input must be a number..."
     }
+
     else {
         let multiply = numberTaken * 3;
         let add = multiply + 10;
@@ -22,15 +25,19 @@ function mindGame (numberTaken){
 // /* Problem 2 */ //
 
 function evenOdd (string) {
+
     // This Function will return whether the input string's length is an even or odd.
+
     if (typeof string !== 'string'){
         return "Provide Valid Input. Input must be a string type data..."
     }
+
     else if (string.length % 2 === 0){
         return "even";
     }
+
     else {
-        return "odd"
+        return "odd";
     }
 }
 
@@ -39,14 +46,18 @@ function evenOdd (string) {
 // /* Problem 3 */ //
 
 function isLGSeven (number){
+
     // This Function will return the input and number if it is less than 7 or it will return double of input if it is greater than 7.
+
     if (typeof number !== 'number'){
         return "Provide Valid Input. Input must be a number..."
     }
+
     else if (Math.abs(number - 7) < 7){
         const difference = Math.abs(number - 7);
         return difference;
     }
+
     else {
         return number*2;
     }
@@ -57,10 +68,13 @@ function isLGSeven (number){
 // /* Problem 4 */ //
 
 function findingBadData (dataInArray){
+
     // This Function will return the number of Bad datas in an Array.
+
     if (typeof dataInArray !== 'object'){
         return "Provide Valid Input. Input must be an array..."
     }
+
     const badData = [];
     for (let i = 0; i < dataInArray.length; i++){
         const element = dataInArray[i];
@@ -70,7 +84,30 @@ function findingBadData (dataInArray){
     }
     return badData.length;
 }
-const result = findingBadData ([5, 4, 3, 2, 1, 0, -1, -2, -3, -5, -7, -9]);
-console.log(result);
 
 
+
+// /* Problem 5 */ //
+
+function gemsToDiamond (friend1Gems, friend2Gems, friend3Gems){
+
+    // This Function will convert the number of Gems of three friends into Diamonds.
+
+    if (typeof friend1Gems !== 'number' || typeof friend2Gems !== 'number' || typeof friend3Gems !== 'number'){
+        return "Provide All The Valid Inputs. Input must be numbers..."
+    }
+
+    let friend1Diamond = friend1Gems * 21;
+    let friend2Diamond = friend2Gems * 32;
+    let friend3Diamond = friend3Gems * 43;
+
+    const totalDiamonds = friend1Diamond + friend2Diamond + friend3Diamond;
+
+    if (totalDiamonds >= (1000*2)){
+        const diamondsLeft = totalDiamonds - 2000;
+        return diamondsLeft;
+    }
+    else {
+        return totalDiamonds;
+    }
+}
